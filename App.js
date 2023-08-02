2382decf470b5c054195f47b6ee38dcaccf76fbe
shara5904 <srss1@aluno.ifal.edu.br>
2023-08-02 16:22:31 -0300

    adicionamos a propriedade idade ao gato

--- App.js
+++ App.js
@@ -5,13 +5,16 @@ import { Text } from 'react-native';
     <>
       <Text>Hello World!</Text> 
       <Text>IFAL</Text>
-      <Gato/>
+      <Gato nome="jubileu" idade= {1}/>
+      <Gato nome="Leonino" idade= {2}/>
+      <Gato nome="Marie" idade= {3}/>
+      
     </>
   );
 }
 
-const Gato = () =>{
-  return <Text>Eu sou um miau.</Text>
+const Gato = ({nome, idade}) =>{
+  return <Text>Eu sou um(a) miau e meu nome é {nome.toUpperCase()} e eu tenho {idade} ano(s).</Text>
 }
 
 export default MyApp;

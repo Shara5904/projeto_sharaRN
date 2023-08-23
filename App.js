34d3b5d2bed7e72d88d716318e50dd33894f49a0
Shara5904 <128625731+Shara5904@users.noreply.github.com>
2023-08-23 15:24:10 -0300

    adiciona o Textinput nome no componente gato

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
-import { Text } from 'react-native';
+import { useState } from 'react';
+import { Text, TextInput } from 'react-native';
 
  const MyApp = () => {
   return (
@@ -8,13 +9,29 @@ import { Text } from 'react-native';
       <Gato nome="jubileu" idade= {1}/>
       <Gato nome="Leonino" idade= {2}/>
       <Gato nome="Marie" idade= {3}/>
+
       
+      <Gato nome="Marie" idade= {12}/>
     </>
   );
 }
 
-const Gato = ({nome, idade}) =>{
-  return <Text>Eu sou um(a) miau e meu nome é {nome.toUpperCase()} e eu tenho {idade} ano(s).</Text>
-}
+const Gato = ({idade}) => {
+    const [nome, SetNome]= useState('');
 
+
+
+
+    return (
+      <>
+        <TextInput
+          onchengeText={(texto) =>SetNome(texto)}
+          defaultValue={nome}
+          placeholder='Informe o nome do gato...'
+          style={{borderWidth: 1, height: 40}}
+      />      
+      <Text>Eu sou um(a) gato(a), meu nome é {nome.toUpperCase()} e eu tenho {idade} {idade === 1 ? "ano" : "anos"}.</Text>
+      </>
+    );
+}
 export default MyApp;
